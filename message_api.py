9145ee7b2e269e6c2b3618dc571d2ed66ec0ab52
Joseph Fiore <98jfiore@gmail.com>
2021-11-01 22:13:45 -0400

    Get endpoint

--- message_api.py
+++ message_api.py
@@ -2,6 +2,23 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
 import ast
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+passw = os.environ.get("PASSWORD")
+user = os.environ.get("USER")
+cnx = mysql.connector.connect(user=user, password=passw, host='127.0.0.1', database='sys')
+cursor = cnx.cursor()
+
+get_convo_query = ("""SELECT senderId, receiverId, messageBody, timeSent
+        FROM messages
+        WHERE (senderId=%s AND receiverId=%s) OR (senderId=%s AND receiverId=%s) ORDER BY timeSent""")
+
+add_message = ("""INSERT INTO messages
+              VALUES (%s, %s, %s, CURRENT_TIMESTAMP, NULL)""")
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,7 +31,35 @@ class Messages(Resource):
     parser.add_argument('contactId', required=True)
 
     args = parser.parse_args()
-    return {'text': 'message', 'userId': args['userId'], 'contactId': args['contactId']}
+
+    resp = {"messages": []}
+
+    cursor.execute(get_convo_query, (args['userId'],args['contactId'],args['contactId'],args['userId'],))
+    
+    for (senderId, receiverId, messageBody, timeSent) in cursor:
+      message = {"text": messageBody,
+                  "sender": senderId,
+                  "receiver": receiverId,
+                  "sendTime": timeSent.strftime("%m/%d/%Y, %H:%M:%S")}
+      resp['messages'].append(message)
+    
+    return resp
+  
+  def post(self):
+    parser = reqparse.RequestParser()
+
+    parser.add_argument('senderId', required=True, help='405')
+    parser.add_argument('receiverId', required=True, help='405')
+    parser.add_argument('messageBody', required=True, help='405')
+
+    args = parser.parse_args()
+
+    try:
+      cursor.execute(add_message, (args['senderId'], args['receiverId'], args['messageBody']))
+      cnx.commit()
+      return {"code": "200"}
+    except:
+      return {"code": "405"}
   pass
 
 api.add_resource(Messages, '/messages')
--- message_api_test.py
+++ message_api_test.py
@@ -6,6 +6,8 @@ import json
 
 from message_api import app as api_app
 
+#MOCK OUT cnx.commit()
+
 @pytest.fixture
 def client():
   db_fd, db_path = tempfile.mkstemp()
@@ -31,4 +33,17 @@ def test_get_messages(client):
 
   rv = client.get('/messages?userId=a&contactId=b')
   response = json.loads(rv.data.decode("utf-8").replace("'", '"'))
-  assert "message" not in response.keys()
+  assert "messages" in response.keys()
+  assert len(response["messages"]) is 0
+
+  rv = client.get('/messages?userId=aaaa&contactId=bbbb')
+  response = json.loads(rv.data.decode("utf-8").replace("'", '"'))
+  assert "messages" in response.keys()
+  assert len(response["messages"]) is 2
+  assert "Hello" in response["messages"][0]["text"]
+  assert "Goodbye" in response["messages"][1]["text"]
+
+def test_post_messages(client):
+  rv = client.post('/messages')
+  response = json.loads(rv.data.decode("utf-8").replace("'", '"'))
+  assert "405" in response["message"]["senderId"]
